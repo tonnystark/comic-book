c8a66a6ba92c7ee02cbf9e57082826684ab9b652
Muni Robert Jr <thachmuni1996@gmail.com>
2016-10-14 17:36:33 +0700

    add content folder

--- ComicBook/Controllers/ComicBooksController.cs
+++ ComicBook/Controllers/ComicBooksController.cs
@@ -10,8 +10,20 @@ namespace ComicBook.Controllers
     {
         public ActionResult Detail()
         {
-            return View();         
+            ViewBag.seriesTitle = "The Amazing Spider-Man";
+            ViewBag.issueNumber = 700;
+            ViewBag.description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
+            ViewBag.artists = new string[]
+            {
+        "Script: Dan Slott",
+        "Pencils: Humberto Ramos",
+        "Inks: Victor Olazaba",
+        "Colors: Edgar Delgado",
+        "Letters: Chris Eliopoulos"
+            };
+
+            return View();
         }
-        
+
     }
 }
