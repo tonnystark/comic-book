810d0bda04e61b1dea46ca989af866e61e886ad9
Muni Robert Jr <thachmuni1996@gmail.com>
2016-10-14 21:55:09 +0700

    add class repository

--- ComicBook/Controllers/ComicBooksController.cs
+++ ComicBook/Controllers/ComicBooksController.cs
@@ -3,27 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ComicBook.Models;
+using ComicBook.Data;
 
 namespace ComicBook.Controllers
 {
     public class ComicBooksController : Controller
     {
-        public ActionResult Detail()
+        private ComicBookRepository _comicBookRepository = null;
+        public ComicBooksController()
         {
-            ViewBag.seriesTitle = "The Amazing Spider-Man";
-            ViewBag.issueNumber = 700;
-            ViewBag.description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
-            ViewBag.artists = new string[]
-            {
-        "Script: Dan Slott",
-        "Pencils: Humberto Ramos",
-        "Inks: Victor Olazaba",
-        "Colors: Edgar Delgado",
-        "Letters: Chris Eliopoulos"
-            };
-
-            return View();
+            _comicBookRepository = new ComicBookRepository();
         }
-
+        public ActionResult Detail(int? id)
+        {
+            if(id == null)
+            {
+                return HttpNotFound();
+            }
+            var comicBook = _comicBookRepository.GetComicBook((int)id);
+            return View(comicBook);
+        }
     }
 }
---
+++ ComicBook/Models/ComicBooks.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBook.Models
+{
+    public class ComicBooks
+    {
+        public int ID { get; set; }
+        public string SeriesTitle { get; set; }
+        public int IssueNumber { get; set; }
+        public string DescriptionHtml { get; set; }
+        public Artist[] Artist { get; set; }
+        public bool Favorite { get; set; }
+
+        public string DisplayText
+        {
+            get
+            {
+                return SeriesTitle + " #" + IssueNumber;
+            }
+        }
+        // seria-title-1212.jpg
+        public string CoverImageFileName
+        {
+            get
+            {
+              
+                return SeriesTitle.Replace(" ", "-").ToLower() + "-" + IssueNumber + ".jpg"; ;
+            }
+        }
+    }
+}
