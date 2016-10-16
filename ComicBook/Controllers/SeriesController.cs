a9a11be6d0fc3da33950bcb8822f721035e7309f
Muni Robert Jr <thachmuni1996@gmail.com>
2016-10-16 21:25:31 +0700

    Project competed

---
+++ ComicBook/Controllers/SeriesController.cs
@@ -0,0 +1,36 @@
+﻿using ComicBook.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComicBook.Controllers
+{
+    public class SeriesController : Controller
+    {
+        SeriesRepository _seriesRepositor = null;
+
+        // GET: Series
+        public ActionResult Index()
+        {
+            var series = _seriesRepositor.GetSeries();
+            return View(series);
+        }
+        public SeriesController()
+        {
+            _seriesRepositor = new SeriesRepository();
+        }
+
+        public ActionResult Detail(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            var seriesDetail = _seriesRepositor.GetSeriesDetail((int)id);
+            return View(seriesDetail);
+        }
+
+    }
+}
